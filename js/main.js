6c0602ac99f4051934fc4731e872e8b4487614f7
Abdullah Alawa <awamah.group@gmail.com>
2024-03-13 18:29:36 +0300

    Finish search function + fix display products function

--- js/main.js
+++ js/main.js
@@ -14,9 +14,10 @@ var productList = [];
 if (localStorage.getItem("ourProducts") != null) {
   productList = JSON.parse(localStorage.getItem("ourProducts"));
 
-  displayProduct();
+  displayProduct(productList);
 }
 
+//add new product
 function addProduct() {
   var product = {
     productName: productNameInput.value,
@@ -31,16 +32,33 @@ function addProduct() {
   // save the data on the local storage
   localStorage.setItem("ourProducts", JSON.stringify(productList));
 
-  displayProduct();
+  displayProduct(productList);
   resetProductInputs();
 }
 
+//delete product
 function deleteProduct(deleteIndex) {
   productList.splice(deleteIndex, 1);
   localStorage.setItem("ourProducts", JSON.stringify(productList));
-  displayProduct();
+  displayProduct(productList);
 }
 
+//Search product
+function searchProduct(str) {
+  var filteredProducts = [];
+
+  for (var i = 0; i < productList.length; i++) {
+    if (
+      productList[i].productName.toLowerCase().includes(str.toLowerCase()) ==
+      true
+    ) {
+      filteredProducts.push(productList[i]);
+    }
+  }
+  displayProduct(filteredProducts);
+}
+
+//Reset All input feilds
 function resetProductInputs() {
   productNameInput.value = null;
   productPriceInput.value = null;
@@ -49,29 +67,30 @@ function resetProductInputs() {
   productImageInput.value = null;
 }
 
-function displayProduct() {
+//Display Products
+function displayProduct(arr) {
   var containerElement = ``;
 
-  for (var i = 0; i < productList.length; i++) {
+  for (var i = 0; i < arr.length; i++) {
     containerElement += `<div class="col">
     <div class="border shadow-sm p-2">
       <div class="image-container mb-5">
         <img
           class="w-100 h-100 object-fit-contain"
-          src="./images/${productList[i].productImage}"
+          src="./images/${arr[i].productImage}"
           alt="Samsung-Galaxy-A54"
         />
       </div>
     
-      <h3 class="fs-5">${productList[i].productName}</h3>
+      <h3 class="fs-5">${arr[i].productName}</h3>
       <p class="fs-6 text-secondary">
-      ${productList[i].productDescription}
+      ${arr[i].productDescription}
       </p>
       <p class="fs-6 text-secondary">
-        <span class="fw-semibold text-black">Category:</span> ${productList[i].productCategory}
+        <span class="fw-semibold text-black">Category:</span> ${arr[i].productCategory}
       </p>
       <div class="d-flex justify-content-between">
-        <p class="fw-semibold"> ${productList[i].productPrice} EGP</p>
+        <p class="fw-semibold"> ${arr[i].productPrice} EGP</p>
         <div>
           <i onclick="deleteProduct(${i})" class="fa-solid fa-trash-can text-danger"></i
           ><i class="fa-solid fa-pen-to-square text-success ms-2"></i>
